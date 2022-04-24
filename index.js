450445dd6a048a9e52f34b7d1d28fb79fbbd120d
Liza Arora <liza.arora@outlook.com>
2022-04-24 14:01:23 +1000

    fundamentals arithmetic lab commit

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+var multiply;
+
+multiply = num1 * num2;
+console.log(multiply);
+
+var random;
+random = Math.floor(Math.random() * 10);
+console.log(random);
+
+
+const num3 = 14;
+const num4 = 5;
+var mod;
+mod = num3 % num4;
+console.log(mod);
+
+var max;
+max = Math.max(5, 10, 15, 19, 20, 4, 8);
+console.log(max);
